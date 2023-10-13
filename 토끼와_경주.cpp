1a09a23f14e52f42eda212201c09a055ad5bddbc
Kim-Ziho <jck1461@naver.com>
2023-10-13 16:16:15 +0900

    토끼와_경주

--- 토끼와_경주.cpp
+++ 토끼와_경주.cpp
@@ -27,12 +27,16 @@ N x M 격자, 초기위치 (1,1)
 #include <set>
 using namespace std;
 
+enum {UP, RIGHT, DOWN, LEFT};
+
 struct Rabbit {
   int total;
   int y;
   int x;
   int id;
   int d;
+
+  Rabbit() {}
 
   Rabbit(int id, int d):
     total(0), y(1), x(1), id(id), d(d) {}
@@ -54,29 +58,170 @@ struct Rabbit {
   }
 };
 
-int main() {
-  //freopen("토끼와_경주.txt", "r", stdin);
+struct Compare : public Rabbit {
+  Compare(const Rabbit& r) : Rabbit(r) {
+    total = r.total;
+    y = r.y;
+    x = r.x;
+    id = r.id;
+    d = r.d;
+  }
+  bool operator<(const Compare& r) const {
+    if (y + x == r.y + r.x) {
+      if (y == r.y) {
+        if (x == r.x) {
+          return id > r.id;
+        }
+        return x > r.x;
+      }
+      return y > r.y;
+    }
+    return y + x > r.y + r.x;
+  }
+};
 
+void ready(int id, int d);
+void play(int K, int S);
+void change_dist(int id, int d);
+int getBestRabbit();
+
+set<Rabbit> rabbits;
+int N, M;
+
+void ready(int id, int d) {
+  rabbits.insert(Rabbit(id, d));
+}
+
+Compare jump(Rabbit rabbit, int direct) {
+  int dist = rabbit.d;
+  int y = rabbit.y;
+  int x = rabbit.x;
+  while(true) {
+    int flag = 0;
+    switch(direct) {
+      case UP:
+        if (y - dist <= 0) {
+          dist -= y;
+          y = 2;
+          direct = DOWN;
+        }
+        else {
+          y -= dist;
+          flag = 1;
+        }
+        break;
+      case DOWN:
+        if (y + dist > N) {
+          dist -= N - y + 1;
+          y = N - 1;
+          direct = UP;
+        }
+        else {
+          y += dist;
+          flag = 1;
+        }
+        break;
+      case LEFT:
+        if (x - dist <= 0) {
+          dist -= x;
+          x = 2;
+          direct = RIGHT;
+        }
+        else {
+          x -= dist;
+          flag = 1;
+        }
+        break;
+      case RIGHT:
+        if (x + dist > N) {
+          dist -= N - x + 1;
+          x = N - 1;
+          direct = LEFT;
+        }
+        else {
+          x += dist;
+          flag = 1;
+        }
+        break;
+    }
+    if (flag) break;
+  }
+  rabbit.y = y;
+  rabbit.x = x;
+  rabbit.total += rabbit.d;
+  Compare result = Compare(rabbit);
+  return result;
+}
+
+void play(int K, int S) {
+  set<Compare> picked_rabbits;
+
+  for (int i = 0; i < K; i++) {
+    Rabbit pick = *rabbits.begin();
+    set<Compare> jumped_rabbits;
+
+    for (int i = 0; i < 4; i++) {
+      jumped_rabbits.insert(jump(pick, i));
+    }
+  }
+}
+
+int main() {
+  Rabbit test = Rabbit(10, 2);
+  set<Compare> jumped_rabbits;
+  for (int i = 0; i < 4; i++) {
+    jumped_rabbits.insert(jump(test, i));
+  }
+
+  freopen("토끼와_경주.txt", "r", stdin);
+
+  // int Q;
+  // cin >> Q;
+
+  // for (int i = 0; i < Q; i++) {
+  //   int cmd;
+  //   cin >> cmd;
+
+  //   switch(cmd) {
+  //     case 100:
+  //       int P;
+  //       cin >> N >> M >> P;
+  //       for (int i = 0; i < P; i++) {
+  //         int id, d;
+  //         cin >> id >> d;
+  //         ready(id, d);
+  //       }
+  //       break;
+  //     case 200:
+  //       int K, S;
+  //       cin >> K >> S;
+  //       play(K, S);
+  //       break;
+  //     case 300:
+  //       int id, L;
+  //       cin >> id >> L;
+  //       change_dist(id, L);
+  //       break;
+  //     case 400:
+  //       cout << getBestRabbit() << endl;
+  //       break;
+  //   }
+  // }
+
+
+}
+
+void junk() {
   set<Rabbit> rabbits;
   rabbits.insert(Rabbit(10, 2));
   rabbits.insert(Rabbit(20, 5));
 
   Rabbit pick = *rabbits.begin();
   pick.y = 2;
-  // pick.total += pick.d;
   rabbits.erase(rabbits.begin());
   rabbits.insert(pick);
-
-  vector<Rabbit> v;
-  v.push_back(Rabbit(10, 2));
-  v.push_back(Rabbit(20, 5));
-  v.begin()->y = 2;
-  for (auto& iter : v) {
-    cout << iter.id << ' ' << iter.y << endl;
-  }
 
   for (auto& iter : rabbits) {
     cout << iter.total << ' ' << iter.y + iter.x << ' ' << iter.id << endl;
   }
-
 }
